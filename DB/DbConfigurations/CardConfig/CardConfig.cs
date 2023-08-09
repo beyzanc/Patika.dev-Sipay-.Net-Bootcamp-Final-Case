c99bc9e0d28a97b5bc1d027c91ca22c8a6ab435b
Beyza Çabuk <beyzancabuk@gmail.com>
2023-08-10 01:57:34 +0300

    Delete DB directory

--- DB/DbConfigurations/CardConfig/CardConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ResiPay.DB.DbConfigurations.CardConfig;
-
-public class CardConfig
-{
-    public string Database_Name { get; set; }
-    public string Cards_Collection_Name { get; set; }
-    public string Connection_String { get; set; }
-
-}
